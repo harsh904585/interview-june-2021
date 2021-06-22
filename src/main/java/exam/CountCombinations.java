3dc2eb6b549a8df5d079e6d85702e0c1773a8965
harsh904585 <74758821+harsh904585@users.noreply.github.com>
2021-06-22 12:30:49 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -3,7 +3,36 @@ package exam;
 public class CountCombinations {
 
   public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+   int MAX_POINT = 3;
+        if (k == 0)
+        {
+            printArray(arr, i);
+        }
+        else if(n > 0)
+        {
+            for (int k = 1; k <= MAX_POINT; k++)
+            {
+                arr[i]= k;
+                printCompositions(arr, n-k, i+1);
+            }
+        }
+    }
+     
+    // Utility function to print array arr[]
+    static void printArray(int arr[], int m)
+    {
+        for (int i = 0; i < m; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+   public static void main (String[] args)
+    {
+        int n = 5;
+        int size = 100;
+        int[] arr = new int[size];
+        System.out.println("Different compositions formed by 1, 2 and 3 of "+ n + " are");
+        printCompositions(arr, n, 0);
+    }
     return 0;
   }
 
